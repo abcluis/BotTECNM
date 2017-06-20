7f3d7172691075aceef69ab1758c0cbaa5f209d9
abcluis <abc_luis30@hotmail.com>
2017-06-20 15:02:06 -0600

    post test

--- server.js
+++ server.js
@@ -8,8 +8,15 @@ app.listen(port, function(err) {
     console.log('Chatfuel Bot-Server listening on port 80...');
 });
 
-app.get('/*', function(req, res) {
+app.get('/', function(req, res) {
     var jsonResponse = [];
     jsonResponse.push({ "text": "Hi. " + (Math.random() * 5 + 1).toFixed(0) + " is a lucky number..." });
     res.send(jsonResponse);
 });
+
+app.post('/post', function(req,res){
+    var jsonResponse = [];
+    console.log(req);
+    jsonResponse.push({ "text": "Hola. " + (Math.random() * 5 + 1).toFixed(0) + " es tu numero de la suerte..." });
+    res.send(jsonResponse);
+})
