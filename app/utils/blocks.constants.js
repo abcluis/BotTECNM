e1e35c7f0d102c48951b0941a71b8620982b7ea0
abcluis <abc_luis30@hotmail.com>
2017-06-22 17:09:42 -0600

    add blocks ....

--- app/utils/blocks.constants.js
+++ app/utils/blocks.constants.js
@@ -4,6 +4,23 @@
 module.exports = {
 
     BLOCK_SCHOOL : 'USER Input',
-    BLOCK_DATEGRAD: 'Fecha Graduacion'
-    
+    BLOCK_DATEGRAD: 'Fecha Graduacion',
+    BLOCK_FULL_NAME: 'NombreCompleto',
+    BLOCK_NUMBER_CONTROL: 'NumeroControl',
+    BLOCK_BIRTHDATE: 'FechaNacimiento',
+    BLOCK_CURP: 'CURP',
+    BLOCK_GENDER: 'Sexo',
+    BLOCK_STATUS : 'EstadoCivil',
+    BLOCK_ADDRESS : 'Domicilio',
+    BLOCK_CITY : 'Ciudad',
+    BLOCK_MUNICIPIO : 'Municipio',
+    BLOCK_STATE : 'Estado',
+    BLOCK_PHONE : 'Telefono',
+    BLOCK_EMAIL : 'E-mail',
+    BLOCK_PHONE_ALT : 'TelefonoCasaPaterna',
+    BLOCK_SPECIALTY : 'CarreraEgresoEspecialidad',
+    BLOCK_MONTH_GRADUATE : 'MesAnhoEgreso',
+    BLOCK_TITLED : 'Titulado',
+    BLOCK_LANGUAGE : 'DominioIdioma',
+    BLOCK_PACKAGE_COMP : 'ManejoPaquetesComp'
 };
