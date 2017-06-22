82fe57afffa882a34a340eaffbe8a66d4de82401
MarioAGR <marioagr95@outlook.com>
2017-06-21 21:01:08 -0600

    Little test

--- app/controllers/bot.controller.js
+++ app/controllers/bot.controller.js
@@ -57,9 +57,10 @@ function registerUser(req,res) {
                         let card = templates.createCard('Hola esto es una carta de prueba');
 
                         let btn1 = templates.createButtonBlock(blocks.BLOCK_SCHOOL,'Desde el inicio');
-                        let btn2 = templates.createButtonBlock(blocks.BLOCK_DATEGRAD,'Desde la fecha de graduacion');
+                        let btn2 = templates.createButtonBlock(blocks.BLOCK_DATEGRAD,'Desde fecha de graduacion');
 
-                        card.attachment.payload.buttons.push(btn1, btn2);
+                        //card.attachment.payload.buttons.push(btn1, btn2);
+                        text.attachment.payload.buttons.push(btn1, btn2);
                         response.messages.push(text);
                         response.messages.push(card);
 
