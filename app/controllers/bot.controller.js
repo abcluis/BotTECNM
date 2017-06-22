dc9c62d8082282d3f9470309834e815978fb6029
abcluis <abc_luis30@hotmail.com>
2017-06-21 22:07:39 -0600

    refactoring ....

--- app/controllers/bot.controller.js
+++ app/controllers/bot.controller.js
@@ -3,6 +3,7 @@
  */
 
 const userService = require('../services/user.service');
+const School          = require('../models/school.model');
 const templates = require('../templates');
 const blocks = require('../utils/blocks.constants');
 
@@ -110,22 +111,20 @@ function registerSchool(req,res){
     School.findOne({'nicks.name': req.body.school})
         .then((result) => {
             if (result) {
-
-                let body = templates.bodyChat();
-                let card = templates.cardChat('Asi que eres del ' + result.name);
-                let btnYes = templates.buttonBlockChat('Yes', blocks.BLOCK_DATEGRAD);
-                let btnNo = templates.buttonBlockChat('No', blocks.BLOCK_SCHOOL);
-
+                let body = new templates.bodyChat();
+                let card = new templates.cardChat('Asi que eres del ' + result.name);
+                let btnYes = new templates.buttonBlockChat('Yes', blocks.BLOCK_DATEGRAD);
+                let btnNo = new templates.buttonBlockChat('No', blocks.BLOCK_SCHOOL);
                 card.addButton(btnYes);
                 card.addButton(btnNo);
                 body.add(card);
-
+                console.log(body.content);
                 res.send(body.content);
                 
             }else {
-                let body = templates.bodyChat();
-                let card = templates.cardChat('Puedes volver a ingresar la informacion');
-                let btnOk = templates.buttonBlockChat('OK', blocks.BLOCK_SCHOOL);
+                let body = new templates.bodyChat();
+                let card = new templates.cardChat('Puedes volver a ingresar la informacion');
+                let btnOk = new templates.buttonBlockChat('OK', blocks.BLOCK_SCHOOL);
 
                 card.addButton(btnOk);
                 body.add(card);
